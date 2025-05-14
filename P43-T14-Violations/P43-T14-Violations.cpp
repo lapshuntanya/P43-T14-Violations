98a431b2c4e1ba2a0699e76818fe77cecf084445
lapshun_t_s <lapshun.t.s@gmail.com>
2025-05-14 17:25:58 +0300

    Добавьте файлы проекта.

---
+++ P43-T14-Violations/P43-T14-Violations.cpp
@@ -0,0 +1,17 @@
+﻿#include "Date.h"
+
+int main()
+{
+	Date my_date = { 15, 8, 2000 };
+
+	Date now; //0 0 0
+	now.getNowDate();
+
+	my_date.printDate();
+	now.printDate();
+	cout << "================================\n";
+	cout << my_date.toString() << endl;
+	cout << my_date.toString("%y-%m-%d") << endl;
+	cout << my_date.toString("%d %b %Y") << endl;
+}
+
---
+++ P43-T14-Violations/Violation.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Date.h"
+
+struct  Violation
+{
+	Date date_viol;			//Дата порушення
+	char info[100] = "";	//Опис порушення
+	float fine = 0;			//Сума штрафу
+
+
+
+};
