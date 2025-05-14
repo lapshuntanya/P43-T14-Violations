a806a7155b02d6308ef17ab7b692b184f672d361
lapshun_t_s <lapshun.t.s@gmail.com>
2025-05-14 17:44:14 +0300

    main

--- P43-T14-Violations/P43-T14-Violations.cpp
+++ P43-T14-Violations/P43-T14-Violations.cpp
@@ -1,8 +1,10 @@
 ﻿#include "Date.h"
+#include "Violation.h"
+#include <Windows.h>
 
 int main()
 {
-	Date my_date = { 15, 8, 2000 };
+	/*Date my_date = {15, 8, 2000};
 
 	Date now; //0 0 0
 	now.getNowDate();
@@ -12,6 +14,18 @@ int main()
 	cout << "================================\n";
 	cout << my_date.toString() << endl;
 	cout << my_date.toString("%y-%m-%d") << endl;
-	cout << my_date.toString("%d %b %Y") << endl;
+	cout << my_date.toString("%d %b %Y") << endl;*/
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Violation a{ {1, 1, 2000}, "Перевищення швидкости 200 км/год", 15'000 };
+	Violation b; // {0, 0, 0}, "", 0
+	b.fillViol();
+
+	cout << "================================\n";
+	a.printViol();
+	b.printViol();
+
 }
 
